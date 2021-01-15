bf5e1d82eca3462ec0fe9d29e6688f71ec4dac44
Wade Pate <wadejp8@gmail.com>
2021-01-15 12:59:26 -0700

    Completed tasks throughout

---
+++ index.js
@@ -0,0 +1,75 @@
+let formBtn = document.getElementById('close-form')
+let form = document.querySelector('.form')
+
+let nameInput = document.getElementById('name')
+let emailInput = document.getElementById('email')
+let emailList = []
+
+let formContainer = document.querySelector('.form-container')
+
+let cart = document.createElement('div')
+let main = document.querySelector('.hero')
+let cartItems = 0
+
+const toggler = () => {
+  form.classList.toggle('hide')
+  formBtn.innerText === 'X' ? formBtn.innerText = '+' : formBtn.innerText = 'X'
+}
+
+formBtn.addEventListener('click', toggler)
+
+
+function validateForm() {
+  if(!nameInput.value || !emailInput.value){
+    if(!nameInput.value){
+      nameInput.style.border = '2px solid red'
+      alert('You need to input your name')
+    }
+    if(!emailInput.value){
+      emailInput.style.border = '2px solid red'
+      alert('You need to input your email address')
+    }
+  } else {
+    emailList.push({name: nameInput.value, email: emailInput.value})
+    displayThankYou(removeMessage())
+  }
+}
+
+
+
+form.addEventListener('submit', function(event){
+  event.preventDefault()
+  validateForm()
+})
+
+
+const displayThankYou = () => {
+  formContainer.innerText = 'Thank you for subscribing!'
+}
+
+const removeMessage = () => {
+  setTimeout(function(){
+    formContainer.remove()
+  }, 3000)
+}
+
+nameInput.addEventListener('change', event => {
+  nameInput.style.border = 'none'
+})
+
+emailInput.addEventListener('change', event => {
+  emailInput.style.border = 'none'
+})
+
+
+const addToCart = () => {
+  if(cartItems === 0){
+    cartItems++
+    cart.setAttribute('class', 'cart-display')
+    cart.innerText = `Your Cart: 1 item`
+  } else {
+    cartItems++
+    cart.innerText = `Your Cart: ${cartItems} items`
+  }
+  main.appendChild(cart)
+}
